c8413b85f849ec0f50cbec4e5b289b4067bba2fe
Eric Laribee <larigo11@twc.com>
2020-07-02 22:23:36 -0400

    Add initial code to testRegisterUser

--- teamify/tests/Unit/ControllerTest.php
+++ teamify/tests/Unit/ControllerTest.php
@@ -5,12 +5,19 @@ namespace Tests\Unit;
 
 use App\Http\Controllers\Auth\RegisterController;
 use App\Http\Controllers\Controller;
+use PHPUnit\Util\Printer;
 
 
 class ControllerTest extends \Tests\TestCase
 {
     public function testRegisterNewUser()
     {
+        $user = factory( 'App\User' )->raw();
+
+        $response = $this->post( '/register', $user );
+
+        fwrite(STDERR, $response->content());
+
         $this->assertTrue(true);
     }
 }
